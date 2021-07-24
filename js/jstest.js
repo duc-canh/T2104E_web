681243aecb6f48b2baf92c31b1d1810502ae5e28
duc-canh <84029160+duc-canh@users.noreply.github.com>
2021-07-24 21:17:35 +0700

    test

--- js/jstest.js
+++ js/jstest.js
@@ -5,10 +5,10 @@ function ketthuc(){
     alert("bạn đã hoàn thành survey")
 }
 function show(){
-    var ct ="<div class=\"d1\" style=\"width: 1000px; height: 300px\">\n" +
+    var ct ="<div class=\"d1 \" style=\"width: 1000px; height: 300px\">\n" +
         "        <h2>Matrsimonial Survey</h2>\n" +
         "        <div class=\"main\" style=\"width: 600px;height: 100px\">\n" +
-        "            <form class=\"col-md-12\">\n" +
+        "            <form class=\"col-md-12 form\">\n" +
         "                <div class=\"col-md-3\">how often you are</div>\n" +
         "                <div class=\"col-md-3\">\n" +
         "                    <input type=\"radio\" id=\"a1\" name=\"fav_language\" value=\"HTML\">\n" +
@@ -23,7 +23,7 @@ function show(){
         "                    <label for=\"a\">montly</label>\n" +
         "                </div>\n" +
         "            </form>\n" +
-        "            <form class=\"col-md-12\">\n" +
+        "            <form class=\"col-md-12 form\">\n" +
         "                <div class=\"col-md-3\">where you ues interne</div>\n" +
         "                <div class=\"col-md-3\">\n" +
         "                    <input type=\"radio\" id=\"b1\" name=\"fav_language\" value=\"HTML\">\n" +
